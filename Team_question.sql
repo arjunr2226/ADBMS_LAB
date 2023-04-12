631c4c1ee7560deb7c422095c3168b5efb69d8dd
arjunr2226 <2226@tkmce.ac.in>
2023-04-12 16:07:35 +0530

    updated teamQ

--- Team_question.sql
+++ Team_question.sql
@@ -5,19 +5,58 @@ create table customer(
 	Cname varchar(255)
 );
 create table account(
-	Ano int primary key, 
-	Atype varchar(10) check(Atype like 'S' or 'C'), 
+	Ano varchar(22) primary key, 
+	Atype varchar(10) check(Atype like ('S' or 'C')), 
 	balance int,
 	Cid int, 
 	foreign key(Cid) 
 	references customer(Cid)
 );
+
+INSERT INTO ACCOUNT VALUES('A01', 'S', 20000, 101);
+INSERT INTO ACCOUNT VALUES('A02', 'C', 22200, 102);
+INSERT INTO ACCOUNT VALUES('A03', 'S', 20000, 103);
+INSERT INTO ACCOUNT VALUES('A04', 'C', 50000, 104);
+INSERT INTO ACCOUNT VALUES('A05', 'S', 100000, 105);
+INSERT INTO ACCOUNT VALUES('A06', 'C', 55000, 106);
+INSERT INTO ACCOUNT VALUES('A07', 'S', 45000, 107);
+
+SELECT * FROM ACCOUNT;
+
+DELETE FROM ACCOUNT;
+
 create table transaction(
-	Tid int primary key, 
-    Ano int,
+	Tid varchar(10) primary key, 
+    Ano varchar(10),
     Ttype  varchar(20) check(Ttype like 'D' or 'W'), 
     Tdate date, 
     Tamount int
 );
-CREATE SEQUENCE Customer_Sequence START WITH 1 INCREMENT BY 10;
-ALTER TABLE customer ADD CID INTEGER DEFAULT NEXT VALUE FOR Customer_Sequence;
+
+insert into customer values
+	 (101, "Aswin"), 
+	 (102,"Ayash"),
+	 (103,"Amjad"),
+	 (104,"Hrishi"),
+	 (105,"Nidal"),
+	 (106,"Naina"), 
+	 (107, "Devika");
+
+select * from customer;
+
+insert into transaction(Tid, Ano, Ttype,Tdate, Tamount) values  ('T01', 'A01', 'D', '2019-01-01', 2000),
+('T02', 'A02', 'W', '2020-05-25', 1500), 
+('T03', 'A03', 'D', '2021-03-13', 5000),
+('T04', 'A04', 'W', '2022-07-05', 6000),
+('T05', 'A05', 'D', '2023-04-04', 7500);
+
+SELECT * FROM TRANSACTION;
+
+CREATE VIEW detailsView AS
+SELECT *
+FROM Account
+WHERE Atype in ('S', 'C');
+
+SELECT * FROM DETAILSVIEW;
+
+DELETE FROM TRANSACTION;
